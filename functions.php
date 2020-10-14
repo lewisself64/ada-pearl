3d459096098c784289b1918a385a128dd168f6fa
Lewis Self <lewisself@tutanota.com>
2020-10-14 22:44:06 +0100

    Registered theme footer widget
    Registered theme footer widget.

--- functions.php
+++ functions.php
@@ -136,6 +136,18 @@ function ada_widgets_init() {
 			'after_title'   => '</h2>',
 		)
 	);
+
+	register_sidebar(
+		array(
+			'name'          => esc_html__( 'Footer Widgets', 'ada' ),
+			'id'            => 'footer-widgets-1',
+			'description'   => esc_html__( 'Add widgets here.', 'ada' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		)
+	);
 }
 add_action( 'widgets_init', 'ada_widgets_init' );
 
