7e73b96e7e0eff6fffe5e9d139cfa041b8e244c9
Lewis Self <lewisself@tutanota.com>
2020-10-18 13:30:44 +0100

    Created banner block patterns
    Created block patterns for banners

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -29,6 +29,36 @@ function ada_register_block_patterns() {
 		);
 
 		register_block_pattern(
+			'ada/banner',
+			array(
+				'title'       => __( 'Basic Banner', 'ada' ),
+				'description' => __( 'A Basic Banner', 'ada' ),
+				'content'     => "<!-- wp:cover {\"url\":\"https://s.w.org/images/core/5.5/don-quixote-06.jpg\",\"id\":19,\"overlayColor\":\"black\",\"minHeight\":400,\"contentPosition\":\"center center\",\"align\":\"full\",\"className\":\"banner\"} -->\n<div class=\"wp-block-cover alignfull has-black-background-color has-background-dim is-position-center-center banner\" style=\"background-image:url(https://s.w.org/images/core/5.5/don-quixote-06.jpg);min-height:400px\"><div class=\"wp-block-cover__inner-container\"><!-- wp:heading {\"level\":1} -->\n<h1>Page Title</h1>\n<!-- /wp:heading --></div></div>\n<!-- /wp:cover -->",
+				'categories'  => array( 'banners' ),
+			)
+		);
+
+		register_block_pattern(
+			'ada/banner-description',
+			array(
+				'title'       => __( 'Banner with description', 'ada' ),
+				'description' => __( 'A Banner with a description', 'ada' ),
+				'content'     => "<!-- wp:cover {\"url\":\"https://s.w.org/images/core/5.5/don-quixote-06.jpg\",\"id\":19,\"overlayColor\":\"black\",\"contentPosition\":\"center center\",\"align\":\"full\",\"className\":\"banner-description\"} -->\n<div class=\"wp-block-cover alignfull has-black-background-color has-background-dim is-position-center-center banner-description\" style=\"background-image:url(https://s.w.org/images/core/5.5/don-quixote-06.jpg)\"><div class=\"wp-block-cover__inner-container\"><!-- wp:heading {\"level\":1} -->\n<h1>Page Title</h1>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Vestibulum quis turpis non nulla commodo mattis a et justo. Suspendisse nulla sapien, scelerisque vel varius id, auctor vitae lacus. Nulla vehicula libero eu consequat mattis. Vestibulum posuere, ante eget commodo convallis, risus felis pellentesque lacus, at vehicula arcu nulla ac arcu. Nulla tincidunt nibh in massa euismod sodales. In pellentesque metus dolor, condimentum cursus elit posuere sit amet. Nullam nec facilisis metus.</p>\n<!-- /wp:paragraph --></div></div>\n<!-- /wp:cover -->",
+				'categories'  => array( 'banners' ),
+			)
+		);
+
+		register_block_pattern(
+			'ada/banner-description-button',
+			array(
+				'title'       => __( 'Banner with description and button', 'ada' ),
+				'description' => __( 'A Banner with a description and button', 'ada' ),
+				'content'     => "<!-- wp:cover {\"url\":\"https://s.w.org/images/core/5.5/don-quixote-06.jpg\",\"id\":19,\"overlayColor\":\"black\",\"contentPosition\":\"center center\",\"align\":\"full\",\"className\":\"banner-description-button\"} -->\n<div class=\"wp-block-cover alignfull has-black-background-color has-background-dim is-position-center-center banner-description-button\" style=\"background-image:url(https://s.w.org/images/core/5.5/don-quixote-06.jpg)\"><div class=\"wp-block-cover__inner-container\"><!-- wp:heading {\"level\":1,\"style\":{\"typography\":{\"fontSize\":42}}} -->\n<h1 style=\"font-size:42px\">Page Title</h1>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Vestibulum quis turpis non nulla commodo mattis a et justo. Suspendisse nulla sapien, scelerisque vel varius id, auctor vitae lacus. Nulla vehicula libero eu consequat mattis. Vestibulum posuere, ante eget commodo convallis, risus felis pellentesque lacus, at vehicula arcu nulla ac arcu. Nulla tincidunt nibh in massa euismod sodales. In pellentesque metus dolor, condimentum cursus elit posuere sit amet. Nullam nec facilisis metus.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons -->\n<div class=\"wp-block-buttons\"><!-- wp:button {\"borderRadius\":0,\"textColor\":\"white\",\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-white-color has-text-color no-border-radius\">Read More</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:cover -->",
+				'categories'  => array( 'banners' ),
+			)
+		);
+
+		register_block_pattern(
 			'ada/call-to-action',
 			array(
 				'title'       => __( 'Basic Call To Action', 'ada' ),
