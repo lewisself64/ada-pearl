85139f7f7e4582c1697ef13dcd6b52c4fbdaacf2
Lewis Self <lewisself@tutanota.com>
2020-10-18 13:29:07 +0100

    Added missing class to call to action pattern
    Added a missing class to the call to action pattern block.

--- inc/block-patterns.php
+++ inc/block-patterns.php
@@ -1,5 +1,15 @@
 <?php
+/**
+ * Sample implementation of the Custom Header feature
+ *
+ * @link https://developer.wordpress.org/block-editor/developers/block-api/block-patterns/
+ *
+ * @package Ada
+ */
 
+/**
+ * Create custom block patterns to complement the theme.
+ */
 function ada_register_block_patterns() {
 
 	if ( class_exists( 'WP_Block_Patterns_Registry' ) ) {
@@ -26,7 +36,7 @@ function ada_register_block_patterns() {
 			array(
 				'title'       => __( 'Cover Call To Action', 'ada' ),
 				'description' => __( 'Cover Call To Action block with title, description and button', 'ada' ),
-				'content'     => "<!-- wp:cover {\"url\":\"https://s.w.org/images/core/5.5/don-quixote-06.jpg\",\"id\":19,\"hasParallax\":true,\"align\":\"full\"} -->\n<div class=\"wp-block-cover alignfull has-background-dim has-parallax\" style=\"background-image:url(https://s.w.org/images/core/5.5/don-quixote-06.jpg)\"><div class=\"wp-block-cover__inner-container\"><!-- wp:heading {\"style\":{\"typography\":{\"fontSize\":32}}} -->\n<h2 style=\"font-size:32px\">Call To Action</h2>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Vivamus ullamcorper neque et metus ultricies, at convallis ex convallis. Pellentesque sem sapien, suscipit sed euismod vel, interdum ultricies nulla. Fusce maximus est ut neque euismod semper. Nulla massa urna, aliquam sed mi ac, pellentesque efficitur purus.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons -->\n<div class=\"wp-block-buttons\"><!-- wp:button {\"borderRadius\":0,\"textColor\":\"white\",\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-white-color has-text-color no-border-radius\">Read More</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:cover -->",
+				'content'     => "<!-- wp:cover {\"url\":\"https://s.w.org/images/core/5.5/don-quixote-06.jpg\",\"id\":19,\"hasParallax\":true,\"align\":\"full\",\"className\":\"cover-call-to-action\"} -->\n<div class=\"wp-block-cover alignfull has-background-dim has-parallax cover-call-to-action\" style=\"background-image:url(https://s.w.org/images/core/5.5/don-quixote-06.jpg)\"><div class=\"wp-block-cover__inner-container\"><!-- wp:heading {\"style\":{\"typography\":{\"fontSize\":32}}} -->\n<h2 style=\"font-size:32px\">Call To Action</h2>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Vivamus ullamcorper neque et metus ultricies, at convallis ex convallis. Pellentesque sem sapien, suscipit sed euismod vel, interdum ultricies nulla. Fusce maximus est ut neque euismod semper. Nulla massa urna, aliquam sed mi ac, pellentesque efficitur purus.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons -->\n<div class=\"wp-block-buttons\"><!-- wp:button {\"borderRadius\":0,\"textColor\":\"white\",\"className\":\"is-style-outline\"} -->\n<div class=\"wp-block-button is-style-outline\"><a class=\"wp-block-button__link has-white-color has-text-color no-border-radius\">Read More</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:cover -->",
 				'categories'  => array( 'call-to-actions' ),
 			)
 		);
