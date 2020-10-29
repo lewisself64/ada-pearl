9f40cdb8aad992722ccf575aece64bba43b61ecf
Lewis Self <lewisself@tutanota.com>
2020-10-29 17:37:29 +0000

    Move title outside of main
    Move the title outside of main element.

--- archive.php
+++ archive.php
@@ -10,16 +10,16 @@
 get_header();
 ?>
 
+	<header class="page-header">
+		<?php
+		the_archive_title( '<h1 class="page-title">', '</h1>' );
+		the_archive_description( '<div class="archive-description">', '</div>' );
+		?>
+	</header><!-- .page-header -->
+
 	<main id="primary" class="site-main">
 
 		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
