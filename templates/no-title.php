4e70f55ac7c6db2d4e2aaf2b9404af4bd870a0ae
Lewis Self <lewisself@tutanota.com>
2020-12-11 23:25:16 +0000

    Removed incorrect template description
    Removed incorrect template description

--- templates/no-title.php
+++ templates/no-title.php
@@ -1,14 +1,9 @@
 <?php
 
-/* Template Name: No title */
-
 /**
- * The template for displaying all pages without titles
+ * Template Name: No title
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
+ * The template for displaying all pages without titles
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
